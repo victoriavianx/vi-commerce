f3348d217b87428fbc600e5691876329dbdcbfc8
Victoria Viana <victoriagarcia963@gmail.com>
2025-07-27 18:04:43 -0300

    feature: adding menu with selection options and login and cart icons

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 defineProps<{
   name: string;
+  customStyle?: string;
 }>();
 </script>
 
 <template>
-  <div>
+  <div :class="customStyle">
     <span class="material-symbols-outlined">{{ name }}</span>
   </div>
 </template>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -11,13 +11,27 @@ const menu: item[] = [
 </script>
 
 <template>
-  <header id="header" class="w-full max-w-7xl">
+  <header id="header" class="w-full max-w-7xl h-20">
     <nav id="menu" class="flex flex-row justify-around items-center h-20">
-      <h1 id="title" class="font-bold tracking-wider">VICOMMERCE</h1>
-      <ul id="list" class="flex flex-row">
+      <h1 id="title" class="font-bold tracking-wider px-10">VICOMMERCE</h1>
+      <ul id="list" class="flex flex-row w-80 max-w-sm justify-center">
         <li v-for="{ id, name } in menu" :key="id" class="px-5">{{ name }}</li>
       </ul>
-      <Icon name="person" />
+      <div class="w-32 max-w-xs flex flex-row justify-evenly">
+        <Icon name="person" />
+        <Icon name="shopping_cart" />
+      </div>
     </nav>
   </header>
 </template>
+
+<style>
+#header {
+  box-shadow:
+    rgba(240, 46, 170, 0.4) 0px 5px,
+    rgba(240, 46, 170, 0.3) 0px 10px,
+    rgba(240, 46, 170, 0.2) 0px 15px,
+    rgba(240, 46, 170, 0.1) 0px 20px,
+    rgba(240, 46, 170, 0.05) 0px 25px;
+}
+</style>
